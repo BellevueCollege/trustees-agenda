c327d1ee75243eb4f3e890e21cbe356f04012426
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2015-04-15 12:48:20 -0700

    Updated version number

--- agenda-post.php
+++ agenda-post.php
@@ -4,7 +4,7 @@ Plugin Name: trustees-adenda
 Plugin URI: https://github.com/BellevueCollege/trustees-adenda
 Description: This plugin registers the 'Agenda' post type 
 Author: Bellevue College Technology Development and Communications
-Version: 1.0
+Version: 1.0.0.0
 Author URI: http://www.bellevuecollege.edu
 */
 
